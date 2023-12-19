fea5e73415c6ab95c7e3a4647e613c9add4d8894
PUNEETHGIRIBT <153577873+PUNEETHGIRIBT@users.noreply.github.com>
2023-12-19 10:48:18 +0530

    Add files via upload

---
+++ RectangleArea.java
@@ -0,0 +1,13 @@
+class RectangleArea{
+	public static void main(String args[]){
+	int length,breadth;
+	length= Integer.parseInt(args[0]); // command line arguments 
+	breadth = Integer.parseInt(args[1]); // convert string to integer
+		int area = length * breadth;
+			System.out.println("My name is PUNEETH GIRI B T");
+			System.out.println("USN is: 2023BMS02531");
+			System.out.println("length of rectangle=" +length);
+			System.out.println("breadth of rectangle=" +breadth);
+			System.out.println("area of rectangle=" +area);
+		}
+	}
