ebb694c602b492c29ffb691365114c7811f039d2
PUNEETHGIRIBT <153577873+PUNEETHGIRIBT@users.noreply.github.com>
2023-12-27 19:50:53 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld{
-		public static void main(String[] args){
-			System.out.println("My name is PUNEETH GIRI B T");
-			System.out.println("USN is: 2023BMS02531");
-			System.out.println("Hello, World!");
-		}
-}
